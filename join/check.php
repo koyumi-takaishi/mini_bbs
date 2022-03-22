dd520f90bd584226d55e0768c17e3070efcbd97e
koyumi-takaishi <takaishi.takoyaki@gmail.com>
2022-03-22 18:28:40 +0900

    check.phpエラー修正

--- join/check.php
+++ join/check.php
@@ -19,10 +19,9 @@ if (!empty($_POST)) {
 	$statement = $db->prepare('INSERT INTO members SET name=?, email=?,	password=?, picture=?, created=NOW()');
 
 	// executeメソッドで?の箇所に挿入したい内容を指定する
-	echo $ret = $statement->execute(array(
+	$ret = $statement->execute(array(
 		$_SESSION['join']['name'],
 		$_SESSION['join']['email'],
-		// sha1ファンクションでパスワード暗号化
 		sha1($_SESSION['join']['password']),
 		$_SESSION['join']['image']
 	));
@@ -37,6 +36,7 @@ if (!empty($_POST)) {
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -47,38 +47,38 @@ if (!empty($_POST)) {
 </head>
 
 <body>
-<div id="wrap">
-  <div id="head">
-    <h1>会員登録</h1>
-  </div>
-  <div id="content">
-		<form action="" method="post">
-			<input type="hidden" name="action" value="submit" />
-			<dl>
-			<dt>ニックネーム</dt>
-			<dd>
-				<!-- htmlタグとか入力されても文字列として出力する。ENT_QUOTES（シングルクオートとダブルクオートを共に変換） -->
-				<?php echo htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES); ?>
-			</dd>
-			<dt>メールアドレス</dt>
-			<dd>
-				<!-- htmlタグとか入力されても文字列として出力する。ENT_QUOTES（シングルクオートとダブルクオートを共に変換） -->
-				<?php echo htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES); ?>
-			</dd>
-			<dt>パスワード</dt>
-			<dd>
-			【表示されません】
-			</dd>
-			<dt>写真など</dt>
-			<dd>
-				<img src="../member_picture/<?php echo htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES); ?>" width="100" height="100" alt="" />
-			</dd>
-			</dl>
-			<div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input
-			type="submit" value="登録する" /></div>
-		</form>
-  </div>
+	<div id="wrap">
+		<div id="head">
+			<h1>会員登録</h1>
+		</div>
+		<div id="content">
+			<form action="" method="post">
+				<input type="hidden" name="action" value="submit" />
+				<dl>
+					<dt>ニックネーム</dt>
+					<dd>
+						<!-- htmlタグとか入力されても文字列として出力する。ENT_QUOTES（シングルクオートとダブルクオートを共に変換） -->
+						<?php echo htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES); ?>
+					</dd>
+					<dt>メールアドレス</dt>
+					<dd>
+						<!-- htmlタグとか入力されても文字列として出力する。ENT_QUOTES（シングルクオートとダブルクオートを共に変換） -->
+						<?php echo htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES); ?>
+					</dd>
+					<dt>パスワード</dt>
+					<dd>
+						【表示されません】
+					</dd>
+					<dt>写真など</dt>
+					<dd>
+						<img src="../member_picture/<?php echo htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES); ?>" width="100" height="100" alt="" />
+					</dd>
+				</dl>
+				<div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する" /></div>
+			</form>
+		</div>
 
-</div>
+	</div>
 </body>
+
 </html>
